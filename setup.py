6933c20de84cb7d663c911d1bb9b9c0b441cc7b0
Your Name <your.email@example.com>
2025-11-18 10:26:53 +0530

    clean the data as well as performing the EDA as well as model trainng

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'pandas', 'numpy', 'scikit-learn', 'matplotlib', 'seaborn', 'streamlit', 'shap', 'xgboost'
-    ],
+        'pandas', 'numpy', 'scikit-learn', 'matplotlib', 'seaborn', 'streamlit', 'shap', 'xgboost','category_encoders'
+    ]
 )
